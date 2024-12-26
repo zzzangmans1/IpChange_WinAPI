a7aaaa397c3b16f8863f5e6d1edc3c3b667e6444
zzzangmans1 <52357235+zzzangmans1@users.noreply.github.com>
2024-12-26 09:45:16 +0900

    Add files via upload

---
+++ WindowsProject1/WindowsProject1/WindowsProject1/resource.h
@@ -0,0 +1,55 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// WindowsProject1.rc에서 사용되고 있습니다.
+//
+#define IDC_MYICON                      2
+#define MAX_OCTETS                      4
+#define MAX_OCTET_LENGTH                4
+#define IDD_WINDOWSPROJECT1_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WINDOWSPROJECT1             107
+#define IDI_SMALL                       108
+#define IDC_WINDOWSPROJECT1             109
+#define IDR_MAINFRAME                   128
+#define IDB_PNG1                        129
+#define IDI_ICON1                       130
+#define IDI_MYICON						131
+#define IDC_IP1                         1001
+#define IDC_IP2                         1002
+#define IDC_IP3                         1003
+#define IDC_IP4                         1004
+#define IDC_IP_TXT                      1005
+#define IDC_SUBNET1                     2001
+#define IDC_SUBNET2                     2002
+#define IDC_SUBNET3                     2003
+#define IDC_SUBNET4                     2004
+#define IDC_SUBNET_TXT                  2005
+#define IDC_GW1                         3001
+#define IDC_GW2                         3002
+#define IDC_GW3                         3003
+#define IDC_GW4                         3004
+#define IDC_GW_TXT                      3005
+#define IDC_APPLY_BUTTON                4001
+#define IDC_EDIT_BUTTON                 4002
+#define IDC_COMBOBOX                    4003
+#define IDC_DHCP_TXT1                   4004
+#define IDC_DHCP_TXT2                   4005
+#define IDC_INT_NAME                    4006
+#define IDC_DHCHSW_BUTTON               4007
+#define IDC_REFRESH_BUTTON               4008
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
